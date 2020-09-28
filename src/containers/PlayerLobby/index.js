7eca9b9941c727ac56abcdfd1b35c2917f6874b3
harshmetrey <harshmetrey246@gmail.com>
2020-09-28 13:43:39 +0530

    2 files modified

--- src/containers/PlayerLobby/index.js
+++ src/containers/PlayerLobby/index.js
@@ -1,21 +1,29 @@
 import React, { useState, useEffect } from "react";
 
-import { Card, Input, Button } from "@material-ui/core";
+import { Card, Input, Button, Fab } from "@material-ui/core";
+
+import { FileCopy } from '@material-ui/icons';
+
+
 
 function PlayerLobby(props) {
-
   const handleOnSubmit = () => {
-      props.history.push({
-        pathname: "/play-area",
-      });
+    props.history.push({
+      pathname: "/play-area"
+    });
   };
 
   return (
     <div className="wrapper-container-home">
-    <Card align="center">
-      <div>Players in the Lobby</div>
-      <Button color="primary" onClick={handleOnSubmit}>Start Game</Button>
-    </Card>
+      <Card align="center">
+        <div>Players in the Lobby</div>
+        <Button color="primary" onClick={handleOnSubmit}>
+          Start Game
+        </Button>
+      </Card>
+      <Fab color="primary" aria-label="add">
+        Share invite code<FileCopy />
+      </Fab>
     </div>
   );
 }
